054144aa6a24a4ad89928d15864695f1d297f2a6
jolatechno <joseph.touzet@gmail.com>
2020-01-06 23:15:04 +0100

    update

--- generalizer/utils.py
+++ generalizer/utils.py
@@ -1,12 +1,58 @@
 import torch
+from skimage.util.shape import view_as_windows as stride, pad
 
-def Stride(kernel, stride=1, padding=None):
-	pad_length = padding if padding is not None else kernel//(stride*2)
-	def separate(input):
-		padded = torch.cat((torch.zeros(pad_length),
-			input, torch.zeros(pad_length)),
-			0).unsqueeze(0)
-		strided = torch.cat([input.narrow(1, i*stride, kernel)
-			for i in range(len(padded)//stride)], 0)
-		return strided
-	return separate
+"""
+helper functions
+"""
+
+def listify(x, dims=1):
+	if isinstance(x, list) or dims < 0:
+		return x
+	return [x for i in range(dims)]
+
+def extendedLen(x):
+	if isinstance(x, list):
+		return len(x)
+	return -1
+
+def calcPadding(kernel, dilation): #assumming that stride and dilation are equal
+	dim = max(extendedLen(kernel), extendedLen(dilation))
+	kernel, dilation = listify(kernel), listify(dilation)
+
+	if isinstance(kernel, list): #equvalent to isinstance(dilation, list)
+		return [k//2 - s//2 for k, s in zip(kernel, dilation)]
+	return kernel//2 - s//2
+
+"""
+padding functions
+"""
+
+def Pad(padding):
+	return lambda input: pad(input, padding)
+
+"""
+slicing functions
+"""
+
+def Stride(kernel, dilation=1):
+	return lambda input: stride(input, kernel, dilation)
+
+"""
+functions to preper a convolution
+"""
+
+def convPrep(input, kernel, dilation=1, padding=None):
+	padding = padding if padding is not None else calcPadding(kernel, dilation)
+
+	padded = pad(input, padding)
+	strided = stride(input, kernel, dilation)
+
+	return strided
+
+def ConvPrep(input, kernel, dilation=1, padding=None):
+	padding = padding if padding is not None else calcPadding(kernel, dilation)
+
+	Fpad = Pad(padding)
+	Fstride = Stride(kernel, dilation)
+	
+	return lambda input: Fstride(Fpad(input))
