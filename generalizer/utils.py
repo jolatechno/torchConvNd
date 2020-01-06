b9771fee493f043ce4464adff0b502837909d3ee
jolatechno <joseph.touzet@gmail.com>
2020-01-06 19:47:52 +0100

    update

--- generalizer/utils.py
+++ generalizer/utils.py
@@ -4,9 +4,9 @@ def Stride(kernel, stride=1, padding=None):
 	pad_length = padding if padding is not None else kernel//(stride*2)
 	def separate(input):
 		padded = torch.cat((torch.zeros(pad_length),
-							input,
-							torch.zeros(pad_length)), 0).unsqueeze(0)
+			input, torch.zeros(pad_length)),
+			0).unsqueeze(0)
 		strided = torch.cat([input.narrow(1, i*stride, kernel)
-							 for i in range(len(padded)//stride)], 0)
+			for i in range(len(padded)//stride)], 0)
 		return strided
 	return separate
