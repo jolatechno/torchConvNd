b74a2c9421519411df1af01c9d205f6e70edcc17
jolatechno <joseph.touzet@gmail.com>
2020-01-06 20:08:06 +0100

    update

--- generalizer/generalizer.py
+++ generalizer/generalizer.py
@@ -1,4 +1,4 @@
-import utils
+from utils import Stride
 
 from torch import nn
 
@@ -7,10 +7,22 @@ class Generalizer(nn.Modules):
 		self.net = net
 		self.parameters = net.parameters
 
-		self.in_shape, self.out_shape = in_shape, out_shape
+		self.out_shape, self.in_shape = out_shape, in_shape if isinstance(in_shape, list) else [in_shape, in_shape]
+		self.Istride, self.Pstride = Stride(self.in_shape[0], out_shape), Stride(self.in_shape[1], out_shape)
 		
 	def forward(self, input, cost=0):
-		pass
+		strided_input = Istride(input)
+		strided_params = Pstride(self.params)
+		expanded_cost = torch.Tensor([cost]*strided_input.shape[0]).unsqueeze(1)
+
+		inter = cat((strided_input, strided_params, expanded_cost), 1)
+		out = self.net(inter)
+
+		result = out.narrow(1, 0, self.in_shape[0]).flatten()
+		self.params = out.narrow(1, self.in_shape[0], self.in_shape[1]).flatten()
+
+		return result
+
 
 	def reset(self, length):
-		self.params = self
+		self.params = torch.zeros(length//in_shape[0]*in_shape[1])
