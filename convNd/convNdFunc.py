8527e5c8a15b1bba8dbe5c872c3df12841ee4cdc
jolatechno <joseph.touzet@gmail.com>
2020-01-07 20:54:14 +0100

    update

--- convNd/convNdFunc.py
+++ convNd/convNdFunc.py
@@ -7,11 +7,11 @@ import numpy as np
 n-D convolution with arbitrary function
 """
 
-def convNdFunc(input, func, kernel, stride=1, padding=0, padding_mode='constant', padding_value=0):
+def convNdFunc(input, func, kernel, stride=1, padding=0, padding_mode='constant', padding_value=0, *args):
 	in_dim = input.ndim
 
 	pereped, shape = convPrep(input, kernel, stride, padding, padding_mode, padding_value)
-	result = func(pereped)
+	result = func(pereped, *args)
 
 	if result.ndim == in_dim:
 		return result
@@ -21,13 +21,13 @@ class ConvNdFunc(nn.Module):
 	def __init__(self, func, kernel, stride=1, padding=0, padding_mode='constant', padding_value=0):
 		super(ConvNdFunc, self).__init__()
 		self.parameters = func.parameters
-		self.forward = lambda input: convNdFunc(input, func, kernel, stride, padding, padding_mode, padding_value)
+		self.forward = lambda input, *args: convNdFunc(input, func, kernel, stride, padding, padding_mode, padding_value, *args)
 
 """
 n-D convolution with a recurent function
 """
 
-def convNdRec(input, mem, func, kernel, stride=1, padding=0, padding_mode='constant', padding_value=0):
+def convNdRec(input, mem, func, kernel, stride=1, padding=0, padding_mode='constant', padding_value=0, *args):
 	in_dim = input.ndim
 	assert in_dim == mem.ndim, "memory and input dimenssion don't match up"
 
@@ -36,7 +36,7 @@ def convNdRec(input, mem, func, kernel, stride=1, padding=0, padding_mode='const
 
 	pereped, shape = convPrep(input, kernel[0], stride[0], padding[0], padding_mode[0], padding_value[0])
 	mem_pereped, mem_shape = convPrep(input, kernel[1], stride[1], padding[1], padding_mode[1], padding_value[1])
-	result, mem_result = func(pereped, mem_pereped)
+	result, mem_result = func(pereped, mem_pereped, *args)
 
 	if result.ndim == in_dim:
 		result = convPost(result, shape)
@@ -50,4 +50,4 @@ class ConvNdRec(nn.Module):
 	def __init__(self, func, kernel,  stride=1, padding=0, padding_mode='constant', padding_value=0):
 		super(ConvNdRec, self).__init__()
 		self.parameters = func.parameters
-		self.forward = lambda input, mem: convNdRec(input, mem, func, kernel, stride, padding, padding_mode, padding_value)
+		self.forward = lambda input, mem, *args: convNdRec(input, mem, func, kernel, stride, padding, padding_mode, padding_value, *args)
