6aab986c265871718613faa6f7fa1e7271df097f
touzet <joseph.touzet@gmail.com>
2020-01-08 15:37:00 +0100

    Update convNdAuto.py

--- torchConvNd/convNdAuto.py
+++ torchConvNd/convNdAuto.py
@@ -7,10 +7,10 @@ from torch import nn
 n-D convolutional layer with automatic output shape
 """
 
-def convNdAuto(input, func, kernel, dilation=1):
+def convNdAuto(input, func, kernel, dilation=1, padding_mode='constant', padding_value=0, *args):
 	pass
 
 class ConvNdAuto(nn.Module):
-	def __init__(self, func, kernel, dilation=1):
+	def __init__(self, func, kernel, dilation=1, padding_mode='constant', padding_value=0):
 		self.parameters = func.parameters
-		self.forward = lambda input, output_shape, *args: (input, func, kernel, dilation, *args)
+		self.forward = lambda input, output_shape, *args: (input, func, kernel, dilation, padding_mode, padding_value, *args)
