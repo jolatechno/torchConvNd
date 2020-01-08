1374e425f0afe8adb63ef71be98bc915bf1158f0
touzet <joseph.touzet@gmail.com>
2020-01-08 15:33:00 +0100

    Update convNdAuto.py

--- torchConvNd/convNdAuto.py
+++ torchConvNd/convNdAuto.py
@@ -1,1 +1,14 @@
+from .utils.utils import *
+from .convNdFunc import convNdFunc, ConvNdFunc
 
+from torch import nn
+
+def convNdAuto():
+  pass
+
+class ConvNdAuto(nn.Module):
+  def __init__(self):
+    pass
+  
+  def forward(self, input, output_shape, *args):
+    pass
