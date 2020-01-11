5ead4c2d39617f5e02ac18b56a4f20c47f3b19d8
touzet <joseph.touzet@gmail.com>
2020-01-11 17:22:47 +0100

    Update convNdRec.py

--- torchConvNd/convNdRec.py
+++ torchConvNd/convNdRec.py
@@ -4,36 +4,20 @@ from .utils.utils import *
 n-D convolutional network with automatic output shape
 """
 
-def convNdRec(x, hidden, shapes, func, kernel, padding_mode='constant', padding_value=0, max_dilation=3, max_stride_transpose=4, *args):
-	out, hidden_out = x.clone(), np.zeros(0)
-	idx = 0
-	for shape in shapes:
-		stride, dilation, padding, stride_transpose = autoShape(list(out.shape), kernel, shape, max_dilation, max_stride_transpose)
-
-		filled = x.clone()
-		for dim, s in enumerate(listify(stride_transpose, x.ndim)):
-			filled = filled.repeat_interleave(s, dim)
-
-		padded = pad(filled, padding, padding_mode, padding_value)
-		strided = view(padded, kernel, stride, dilation)
-		inter, batch_shape = strided.flatten(0, x.ndim - 1), strided.shape[:x.ndim]
-
-		batch_size = inter.shape[0]
-
-		if idx + batch_size > hidden.shape[0]:
-			dif = idx + batch_size - hidden.shape[0]
-			hidden = torch.cat((hidden, torch.zeros(dif, *hidden.shape[1:])), 0)
-
-		result, hidden[idx:idx + batch_size] = func(inter.unsqueeze(1), hidden.narrow(0, idx, batch_size), *args)
-
-		idx += batch_size
-
-		out = result.reshape(*batch_shape)
-		for dim, s in enumerate(listify(shape, x.ndim)):
-			if out.shape[dim] != s:
-				out = out.narrow(dim, 0, s)
-
-	return out, hidden
+def convNdRec(x, hidden, shape, func, kernel, padding_mode='constant', padding_value=0, max_dilation=3, max_stride_transpose=4, *args):
+	class Func:
+		idx = 0
+		def __call__(self, x, *args):
+			length = x.shape[0]
+			dif = idx + length - hidden.shape[0]
+			if dif > 0:
+				hidden = torch.cat((hidden, torch.zeros(dif, hidden.shape[1:])), 0)
+			
+			out, hidden[idx:idx + length] = func(x, hidden[idx: idx + length], *args)
+			idx += length
+			return out
+	result = 
+	return result, hidden
 
 class ConvNdRec(nn.Module):
 	def __init__(self, func, kernel, padding_mode='constant', padding_value=0, max_dilation=3, max_stride_transpose=4):
