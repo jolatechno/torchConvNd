593112496481940af8cb32facccf9b0ca6e896e2
touzet <joseph.touzet@gmail.com>
2020-01-11 17:57:11 +0100

    Update convNdRec.py

--- torchConvNd/convNdRec.py
+++ torchConvNd/convNdRec.py
@@ -6,17 +6,16 @@ n-D recurent convolutional network
 """
 
 def convNdRec(x, hidden, func, kernel, stride=1, dilation=1, padding=0, stride_transpose=1, padding_mode='constant', padding_value=0, *args):
-	class Func:
-		idx = 0
-		def __call__(self, x, *args):
-			length = x.shape[0]
-			dif = idx + length - hidden.shape[0]
-			if dif > 0:
-				hidden = torch.cat((hidden, torch.zeros(dif, hidden.shape[1:])), 0)
+	idx = [0] # Has to be a list to be modified inside of Func
+	def Func(x, *args):
+		length = x.shape[0]
+		dif = idx[0] + length - hidden.shape[0]
+		if dif > 0:
+			hidden = torch.cat((hidden, torch.zeros(dif, hidden.shape[1:])), 0)
 			
-			out, hidden[idx:idx + length] = func(x, hidden[idx: idx + length], *args)
-			idx += length
-			return out
+		out, hidden[idx[0]:idx[0] + length] = func(x, hidden[idx[0]: idx[0] + length], *args)
+		idx[0] += length
+		return out
 	
 	result = convNdFunc(x, func, kernel, stride, dilation, padding, stride_transpose, padding_mode, padding_value, *args)
 	return result, hidden
